d94ccee91545a8eec423521757aa46504dcda1ef
Gautham Kumar <heygautham@gmail.com>
2016-06-10 14:58:10 +0530

    Finally! Used a different logic!
    Changed constraint values directly and it worked!
    
    Left to do :-
    
    Change size
    Change shape
    Overall polishing

--- Voice Mover/ViewController.swift
+++ Voice Mover/ViewController.swift
@@ -13,25 +13,13 @@ class ViewController: UIViewController, SKTransactionDelegate {
     
     @IBOutlet weak var BlackSquare: UIImageView!
     @IBOutlet weak var RecordButton: UIButton!
-    @IBOutlet var MainView: UIView!
     @IBOutlet weak var ResultLabel: UILabel!
-    
-    var MidX:CGFloat = 0
-    var MidY:CGFloat = 0
-    
-    
+    @IBOutlet weak var YConstraint: NSLayoutConstraint!
+    @IBOutlet weak var XConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        MidX = CGRectGetMidX(MainView.frame)
-        MidY = CGRectGetMidY(MainView.frame)
-        
-        //For debugging purposes
-        print(MidX)
-        print(MidY)
-        
-        self.BlackSquare.center = CGPoint(x: MidX, y: MidY)
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,37 +27,9 @@ class ViewController: UIViewController, SKTransactionDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func OnClickDirectionButton(sender: AnyObject) {
-        
-        if sender.currentTitle == "Up" {
-            self.MidY -= 10
-        }
-        
-        else if sender.currentTitle == "Right" {
-            self.MidX += 10
-        }
-        
-        else if sender.currentTitle == "Down" {
-            self.MidY += 10
-        }
-        
-        else if sender.currentTitle == "Left" {
-            self.MidX -= 10
-        }
-        
-        self.BlackSquare.center = CGPoint(x: self.MidX, y: self.MidY)
-        
-        //For debugging purposes
-        print (self.BlackSquare.frame.midX)
-        
-    }
-
     @IBAction func OnClickRecord(sender: AnyObject) {
         
         self.RecordButton.setTitle("Listening", forState: UIControlState.Normal)
-        
-        //For Debugging
-        self.BlackSquare.center = CGPoint(x: self.MidX, y: self.MidY)
         
         let session = SKSession(URL: NSURL(string: "nmsps://NMDPTRIAL_tajy_many_gmail_com20160421150722@sslsandbox.nmdp.nuancemobility.net:443"), appToken: "25758f3a7d733d7c677a3b52fa0126199a329054d5e92f823bdf44aedf413904b6c83f343b7cb0a2f6ebf5f1ec1b0a874d9adb23840aa5bfe9e96dc92aa7cde0")
         
@@ -83,40 +43,29 @@ class ViewController: UIViewController, SKTransactionDelegate {
     
     func transaction(transaction: SKTransaction!, didReceiveRecognition recognition: SKRecognition!) {
         
+        //To Check what the system recorded
         self.ResultLabel.text = recognition.text
         
-        //For Debugging purposes
-        print(self.MidX)
-        
         if recognition.text == "Up" {
-            self.MidY -= 10
+            self.YConstraint.constant -= 10
         }
             
         else if ResultLabel.text! == "Right" {
-            //For Debugging purposes
-            print("I'm in")
-            self.MidX += 100
+            self.XConstraint.constant += 10
         }
             
         else if recognition.text == "Down" {
-            self.MidY += 10
+            self.YConstraint.constant += 10
         }
             
         else if recognition.text == "Left" {
-            self.MidX -= 10
+            self.XConstraint.constant -= 10
         }
         
-        self.BlackSquare.center = CGPoint(x: MidX, y: MidY)
-        
-        //More Debugging attempts
-        print(self.MidX)
-        
         self.RecordButton.setTitle("Start Listening", forState: UIControlState.Normal)
-        
-        //To check what the system recorded
-        print(self.ResultLabel.text!)
-        
-        
+    
     }
+        
+    
 }
 
