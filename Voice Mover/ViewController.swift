14d3496105a0730cb4fc7acf1b2b728e92d94a3b
Gautham Kumar <heygautham@gmail.com>
2016-06-10 11:46:02 +0530

    Recording is done properly. On recording, MidX and MidY are changed accordingly.
    But, image frame is not updating unless we click one of the 4 directional buttons.
    
    Also, image frame is not updating properly through the viewdidload method

--- Voice Mover/ViewController.swift
+++ Voice Mover/ViewController.swift
@@ -7,52 +7,116 @@
 //
 
 import UIKit
+import SpeechKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SKTransactionDelegate {
+    
     @IBOutlet weak var BlackSquare: UIImageView!
-    
+    @IBOutlet weak var RecordButton: UIButton!
     @IBOutlet var MainView: UIView!
+    @IBOutlet weak var ResultLabel: UILabel!
     
-    var MinX:CGFloat = 0
-    var MinY:CGFloat = 0
+    var MidX:CGFloat = 0
+    var MidY:CGFloat = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        MinX = CGRectGetMidX(MainView.frame) - 40
-        MinY = CGRectGetMidY(MainView.frame) - 40
+        MidX = CGRectGetMidX(MainView.frame)
+        MidY = CGRectGetMidY(MainView.frame)
         
-        BlackSquare.frame = CGRect(x: self.MinX, y: self.MinY, width: 80, height: 80)
-
+        //For debugging purposes
+        print(MidX)
+        print(MidY)
+        
+        self.BlackSquare.center = CGPoint(x: MidX, y: MidY)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func OnClickDirectionButton(sender: AnyObject) {
         
         if sender.currentTitle == "Up" {
-            MinY -= 10
+            self.MidY -= 10
         }
         
         else if sender.currentTitle == "Right" {
-            MinX += 10
+            self.MidX += 10
         }
         
         else if sender.currentTitle == "Down" {
-            MinY += 10
+            self.MidY += 10
         }
         
         else if sender.currentTitle == "Left" {
-            MinX -= 10
+            self.MidX -= 10
         }
         
-        BlackSquare.frame = CGRect(x: self.MinX, y: self.MinY, width: 80, height: 80)
+        self.BlackSquare.center = CGPoint(x: self.MidX, y: self.MidY)
+        
+        //For debugging purposes
+        print (self.BlackSquare.frame.midX)
         
     }
 
+    @IBAction func OnClickRecord(sender: AnyObject) {
+        
+        self.RecordButton.setTitle("Listening", forState: UIControlState.Normal)
+        
+        //For Debugging
+        self.BlackSquare.center = CGPoint(x: self.MidX, y: self.MidY)
+        
+        let session = SKSession(URL: NSURL(string: "nmsps://NMDPTRIAL_tajy_many_gmail_com20160421150722@sslsandbox.nmdp.nuancemobility.net:443"), appToken: "25758f3a7d733d7c677a3b52fa0126199a329054d5e92f823bdf44aedf413904b6c83f343b7cb0a2f6ebf5f1ec1b0a874d9adb23840aa5bfe9e96dc92aa7cde0")
+        
+        //On Debugging, it was found that the screen resets itself when it undergoes the following command.
+        session.recognizeWithType(SKTransactionSpeechTypeDictation,
+                                  detection: .Short,
+                                  language: "eng-USA",
+                                  delegate: self)
+        
+    }
+    
+    func transaction(transaction: SKTransaction!, didReceiveRecognition recognition: SKRecognition!) {
+        
+        self.ResultLabel.text = recognition.text
+        
+        //For Debugging purposes
+        print(self.MidX)
+        
+        if recognition.text == "Up" {
+            self.MidY -= 10
+        }
+            
+        else if ResultLabel.text! == "Right" {
+            //For Debugging purposes
+            print("I'm in")
+            self.MidX += 100
+        }
+            
+        else if recognition.text == "Down" {
+            self.MidY += 10
+        }
+            
+        else if recognition.text == "Left" {
+            self.MidX -= 10
+        }
+        
+        self.BlackSquare.center = CGPoint(x: MidX, y: MidY)
+        
+        //More Debugging attempts
+        print(self.MidX)
+        
+        self.RecordButton.setTitle("Start Listening", forState: UIControlState.Normal)
+        
+        //To check what the system recorded
+        print(self.ResultLabel.text!)
+        
+        
+    }
 }
 
